17864979220e9a3724f4f962c5ed4550a40ed726
earvin-tech <wsl@Earvin>
2025-07-10 19:52:21 +1000

    chore: add ignores property to eslint.config.js

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -4,7 +4,26 @@ const { defineConfig } = require("eslint/config");
 
 
 module.exports = defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs" 
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.node
+    }, 
+  },
+  { 
+    ignores: [
+      "node_modules",
+    ],
+  },
 ]);
