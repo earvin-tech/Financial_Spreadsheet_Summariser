2e6213b1f012212d55738c23c9328438fe89996d
earvin-tech <wsl@Earvin>
2025-07-10 16:15:19 +1000

    chore: add MongoDB config setup in config directory

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+
+const app = express();
+
+app.use(express.json());
+app.get("/test", (request, response) => {
+    response.send("Hello World, finance bot here");
+});
+
+
+
+
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv");
+const app = require("./index");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+})
