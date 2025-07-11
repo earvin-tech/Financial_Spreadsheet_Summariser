cc200e8158b3ace31f7f971fe239989d6f8aff63
earvin-tech <wsl@Earvin>
2025-07-11 15:11:34 +1000

    test: lefthook pre-commit functionality

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -55,7 +55,11 @@ userSchema.pre("save", async function (next) {
 //Method to compare passwords
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
-};
+}
+
+
+
+
 
 const User = mongoose.model("User", userSchema);
-module.exports = User;
+module.exports = User
