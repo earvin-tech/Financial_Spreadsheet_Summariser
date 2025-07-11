e9edc867c8b952b9cd23508ebc3c3d0758d0a26a
earvin-tech <wsl@Earvin>
2025-07-11 15:39:46 +1000

    chore: configure lefthook pre-commit with ESLint and Prettier

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -48,14 +48,14 @@ userSchema.pre("save", async function (next) {
     this.password = await bcrypt.hash(this.password, salt);
     next();
   } catch (err) {
-    next(err)
+    next(err);
   }
 });
 
 //Method to compare passwords
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
-}
+};
 
-const User = mongoose.model("User", userSchema)
-module.exports = User
+const User = mongoose.model("User", userSchema);
+module.exports = User;
